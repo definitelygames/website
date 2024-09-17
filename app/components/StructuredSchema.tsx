64eabd4b247765c0ffdaf46376ae5e718fddf797
CPU <156097+caseypugh@users.noreply.github.com>
2024-09-17 11:27:29 -0700

    fix schema

--- app/components/StructuredSchema.tsx
+++ app/components/StructuredSchema.tsx
@@ -9,23 +9,23 @@ import {
 	TWITTER_URL,
 	YOUTUBE_URL,
 } from "../lib/const"
+import { Person, WebSite } from "schema-dts"
 
-const casey = {
+const casey: Person = {
 	"@type": "Person",
 	name: "Casey Pugh",
 	description: "Co-founder",
 	url: ["https://www.caseypugh.com/", "https://twitter.com/caseypugh"],
 }
 
-const charlie = {
+const charlie: Person = {
 	"@type": "Person",
 	name: "Charlie Tran",
 	description: "Co-founder",
 	url: ["https://www.charlietran.com/", "https://twitter.com/charlietran"],
 }
 
-const websiteSchema = {
-	"@context": "http://schema.org",
+const websiteSchema: WebSite = {
 	"@type": "WebSite",
 	name: SITE_TITLE,
 	description: SITE_DESCRIPTION,
@@ -34,14 +34,14 @@ const websiteSchema = {
 	author: [casey, charlie],
 }
 
+// https://nextjs.org/docs/app/building-your-application/optimizing/metadata#json-ld
 export default function StructuredSchema() {
 	return (
 		<>
-			<Script
+			<script
 				id="website-schema"
 				type="application/ld+json"
 				dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
-				strategy="beforeInteractive"
 			/>
 		</>
 	)
