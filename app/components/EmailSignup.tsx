4a7e45a5a0fff7941589f36bf1c6ff460d47525a
CPU <156097+caseypugh@users.noreply.github.com>
2024-09-20 17:09:16 -0700

    post to mailchimp direct

--- app/components/EmailSignup.tsx
+++ app/components/EmailSignup.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 
 export default function EmailSignup({ className }: Props) {
-	const [copy, setCopy] = useState("Get email updates")
+	const [copy, setCopy] = useState("Get updates")
 	function onSuccess() {
 		setCopy("Thank you! 👻")
 	}
@@ -18,7 +18,7 @@ export default function EmailSignup({ className }: Props) {
 				"flex flex-col space-y-2 border-b border-primary pb-2 md:flex-row md:space-x-7 md:space-y-0",
 				className,
 			)}>
-			<div className="whitespace-nowrap md:w-36">{copy}</div>
+			<div className="whitespace-nowrap md:w-28">{copy}</div>
 			<MailchimpForm className="flex-1" onSuccess={onSuccess} />
 		</div>
 	)
@@ -34,6 +34,10 @@ const MailchimpForm = ({ className, onSuccess }: FormProps) => {
 	const [pending, setPending] = useState(false)
 
 	const handleSubmit = async (event: FormEvent) => {
+		await new Promise((resolve) => setTimeout(resolve, 1000))
+		setEmail("")
+		onSuccess?.()
+		return
 		event.preventDefault()
 
 		const formData = new FormData()
@@ -65,15 +69,23 @@ const MailchimpForm = ({ className, onSuccess }: FormProps) => {
 	}
 
 	return (
-		<form onSubmit={handleSubmit} method="post" className={classNames("flex flex-row", className)}>
+		<form
+			action="https://games.us8.list-manage.com/subscribe/post?u=b8ceb76496f682016133b8e5e&amp;id=ffc17f3258&amp;f_id=005c7ce0f0"
+			onSubmit={handleSubmit}
+			method="post"
+			target="_blank"
+			className={classNames("flex flex-row", className)}>
 			<input
 				type="email"
+				name="EMAIL"
 				value={email}
 				onChange={(e) => setEmail(e.target.value)}
 				placeholder="you@email.rizz"
 				className={classNames("w-full transition-opacity", pending ? "opacity-50" : "")}
 				required
 			/>
+			<input type="text" name="b_b8ceb76496f682016133b8e5e_ffc17f3258" className="hidden" tabIndex={-1} />
+			<input type="submit" name="subscribe" className="hidden" value="Subscribe" tabIndex={-1} />
 			<button type="submit">
 				<Arrow className={classNames("w-[13px] transition-opacity", pending ? "opacity-50" : "")} />
 			</button>
